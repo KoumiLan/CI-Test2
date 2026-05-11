826b4f2d7edb54590b9029c2d97f052ac383fd66
Administrator <Administrator@WIN-KPCAHV19HDB>
2026-05-11 16:20:22 +0800

    加入專案檔案。

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,14 @@
+using ClassLibrary1;
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test_Add()
+        {
+            var calc = new Class1();
+            Assert.Equal(4, calc.Add(1, 3));
+        }
+    }
+}
